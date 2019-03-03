b545ae2cfcbfedc79608c6dcd2e4ed27783ef138
Martin Gaitan <mgaitan@onapsis.com>
2019-03-03 17:47:44 -0300

    update script de deployment

--- fabfile.py
+++ fabfile.py
@@ -1,18 +1,21 @@
-from fabric.api import run, env, cd
+from fabric.api import run, env, cd, prefix
+from fabric.decorators import roles
 from fabric.contrib.files import append
-from carreros.local_settings import HOST_IP, HOST_USER, VENV
+from carreros.local_settings import HOST_IP, HOST_USER, VENV, PROJECT_PATH, PROJECT_USER
 
 env.hosts = [HOST_IP]
 env.user = HOST_USER
 
 
+@roles(PROJECT_USER)
 def manage(command):
-    run(f"source {VENV}bin/activate")
-    with cd('/projects/carreros'):
+    with prefix(f"source {VENV}bin/activate"), cd(PROJECT_PATH):
         run(f"{VENV}bin/python manage.py {command}")
+
 
 def shell_plus():
     manage('shell_plus')
+
 
 def dbbackup():
     manage('dbbackup -z')
@@ -23,44 +26,35 @@ def dbrestore():
 
 
 def append_to_local_settings(path):
-    run(f"source {VENV}bin/activate")
     with open(path) as ls:
         content = ls.read()
-    with cd('/projects/carreros'):
+    with prefix(f"source  {VENV}bin/activate"), cd(PROJECT_PATH):
         append('./carreros/local_settings.py', f'\n{content}')
 
 
 def rmpyc():
-    with cd('/projects/carreros'):
+    with cd(PROJECT_PATH):
         run("find . -type d -name \"__pycache__\" -exec rm -rf {} \;")
 
 
 def loaddata(fixture):
-    run(f"source {VENV}bin/activate")
-    with cd('/projects/carreros'):
-        run("git pull")
-        run("/virtualenvs/carreros/bin/python manage.py loaddata fixtures/{}".format(fixture))
+    manage("loaddata fixtures/{}".format(fixture))
 
 
 def importar_actas():
-    run(f"source {VENV}bin/activate")
-    with cd('/projects/carreros'):
-        run(f"{VENV}bin/python manage.py importar_actas --include-seen --only-images")
-
+    manage("importar_actas --include-seen --only-images")
 
 
 def deploy():
-    run(f"source {VENV}bin/activate")
-    with cd('/projects/carreros'):
+    with cd(PROJECT_PATH):
         run("git pull")
-        run("supervisorctl restart carreros")
+    run("supervisorctl restart neuquen")
 
 
 def full_deploy():
-    run(f"source {VENV}bin/activate")
-    with cd('/projects/carreros'):
+    with cd(PROJECT_PATH):
         run("git pull")
         run(f"{VENV}bin/pip install -r requirements.txt")
-        run(f"{VENV}bin/python manage.py migrate")
-        run(f"{VENV}bin/python manage.py collectstatic --noinput")
-        run("supervisorctl restart carreros")
+    manage("migrate")
+    manage("collectstatic --noinput")
+    run("supervisorctl restart neuquen")
